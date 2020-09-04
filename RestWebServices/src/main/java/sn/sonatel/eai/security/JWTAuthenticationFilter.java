0baf63efa96ffe339590a6d5f887200f78c62da4
TiDev00 <41845110+TiDev00@users.noreply.github.com>
2020-09-04 23:56:47 +0000

    Update JWTAuthenticationFilter.java

--- RestWebServices/src/main/java/sn/sonatel/eai/security/JWTAuthenticationFilter.java
+++ RestWebServices/src/main/java/sn/sonatel/eai/security/JWTAuthenticationFilter.java
@@ -71,7 +71,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 			 * logService.createLog(log);
 			 */
 			
-			if(!isMemberOfSonatel(appUser.getMatricule(), appUser.getPassword())) {
+			if(isMemberOfSonatel(appUser.getMatricule(), appUser.getPassword())) {
 				
 				return authenticationManager
 						.authenticate(
